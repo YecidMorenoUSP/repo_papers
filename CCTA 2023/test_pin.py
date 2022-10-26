11b56564efed3343950e96c2f95acd5458f07365
YecidMorenoUSP <yecidmoreno@usp.br>
2022-10-26 18:06:47 -0500

    CCTA 2023

---
+++ CCTA 2023/test_pin.py
@@ -0,0 +1,10 @@
+from __future__ import print_function
+import pinocchio
+
+# http://www.mymodelrobot.appspot.com/5674976526991360
+l1 = 0.42
+l2 = 0.42
+l3 = 0.47
+
+print( l2 + l1/2 )
+print( l2/2 )
